9da1fbdd31b92641785cdc4bbd4e7f61079f8025
andydenenberg <andydenenberg@gmail.com>
2011-11-17 18:24:09 -0600

    before adding Gmaps

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114035502) do
+ActiveRecord::Schema.define(:version => 20111117234522) do
 
   create_table "emps", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "address"
+    t.string   "city"
+    t.string   "country"
+    t.string   "phone"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   create_table "users", :force => true do |t|
