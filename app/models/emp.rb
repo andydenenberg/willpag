cbfc65ffe9e332078e8ff747f372ce305df17b9b
andydenenberg <andydenenberg@gmail.com>
2011-11-22 13:34:54 -0600

    fixed yaml config file

--- app/models/emp.rb
+++ app/models/emp.rb
@@ -5,15 +5,15 @@ class Emp < ActiveRecord::Base
     "#{self.address} , #{self.zip} , #{self.city} , #{self.country}"
   end
 
-  config = YAML.load(File.read('../../../desktop/config.yml'))   
+#  config = YAML.load(File.read('../../../desktop/config.yml'))   
   
   # paperclip
   has_attached_file :photo, 
     :storage => :s3 ,
     :bucket => 'andys_paperclip' ,
     :s3_credentials => {
-      :access_key_id =>  ENV['S3_KEY'] || config['access_key_id'] ,
-      :secret_access_key =>  ENV['S3_SECRET'] || config['secret_access_key']
+      :access_key_id =>  ENV['S3_KEY'] # || config['access_key_id'] ,
+      :secret_access_key =>  ENV['S3_SECRET'] # || config['secret_access_key']
     } ,
     :styles => {
       :tiny => "25x25" ,
